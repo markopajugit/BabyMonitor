7a3cfbe9aaf84dfbcf3c63a476ac4d99a24ddc15
Marko Paju <markopaju92@gmail.com>
2025-11-20 23:06:14 +0200

    v1.7.1

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
         // App version - increment this when you update files to force cache refresh
-        const APP_VERSION = '1.6.5';
+        const APP_VERSION = '1.7.1';
 
         const ICONS = {
             'Feed': '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M9 2h6v5h-6zM9 7v14a2 2 0 0 0 2 2h2a2 2 0 0 0 2-2V7H9z"/></svg>',
